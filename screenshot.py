c835afd8fc40bfbfa1deae928249259de48d5235
Ferry Djaja <ferrygun@yahoo.com>
2020-02-26 19:34:59 -0800

    Create screenshot.py

---
+++ screenshot.py
@@ -0,0 +1,16 @@
+import gi
+from gi.repository import Gdk
+
+win = Gdk.get_default_root_window()
+h = win.get_height()
+w = win.get_width()
+
+print ("The size of the window is %d x %d" % (w, h))
+
+pb = Gdk.pixbuf_get_from_window(win, 593, 80, 486, 210)
+
+if (pb != None):
+    pb.savev("screenshot.png","png", (), ())
+    print("Screenshot saved to screenshot.png.")
+else:
+    print("Unable to get the screenshot.")
